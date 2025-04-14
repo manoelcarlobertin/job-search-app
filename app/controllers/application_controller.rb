94f0f4aaf48f5fe6c159c4ae5c5cc03d56354114
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-14 20:06:50 -0300

    apaguei tudo relacionado a home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :autenticate_user!
+  # before_action :autenticate_company! #......................O devise User NÃO SERVIRÁAAA
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
