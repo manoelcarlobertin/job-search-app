f0bb9385733d5d41379ff549d6a4827c4f1fda7b
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-14 20:17:49 -0300

    validei os atributos de Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,8 @@
 class Company < ApplicationRecord
   has_many :vacancies, dependent: :destroy, inverse_of: :company
+
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
+  validates :description, length: { minimum: 3, maximum: 1000 }
 end
