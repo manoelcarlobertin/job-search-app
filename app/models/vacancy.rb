b3c3d4d7b5b2ee882bf10e4f37f33720855ab3d0
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-14 20:24:32 -0300

    validação dos atributos da vaga de emprego

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,12 @@
 class Vacancy < ApplicationRecord
   belongs_to :company
   has_many :applicants, dependent: :destroy, inverse_of: :vacancy
+
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :description, presence: true,  length: { minimum: 10, maximum: 1000 }
+  validates :company, presence: true
+  validates :requeriments, presence: true
+  validates :salary, presence: true
+  validates :available, presence: true
 end
