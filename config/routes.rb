a24f3d35126defc288e445ab29d52a5f64e14e28
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-14 20:30:54 -0300

    criada autenticação da Empresa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :companies
   devise_for :users
   # get "/vacancies" => "vacancies#index", as: :company_root # passou o model + root, ao user logar.
   resources :applicants
